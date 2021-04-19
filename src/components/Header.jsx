d5999af79b6eaa7125170fbc65eb6960f04e62b7
Lambtsa <42414095+Lambtsa@users.noreply.github.com>
2021-04-19 09:46:32 +0200

    update design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import { useOktaAuth } from '@okta/okta-react';
+import { useHistory } from 'react-router-dom';
 
 const Header = () => {
-  const { oktaAuth } = useOktaAuth();
+  const history = useHistory();
+  const { oktaAuth, authState } = useOktaAuth();
+
+  const handleLoginClick = () => {
+    if (authState.isAuthenticated) {
+      oktaAuth.signOut();
+    } else {
+      history.push('/login');
+    }
+  };
 
   return (
     <header className="header">
       <h2 className="header__title">Charlie&apos;s closet</h2>
       <div>
-        <button type="button" onClick={() => oktaAuth.signOut()}>Admin</button>
+        <button type="button" onClick={handleLoginClick}>{authState.isAuthenticated ? 'Se déconnecter' : 'Se connecter'}</button>
       </div>
     </header>
   );
--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { Redirect } from 'react-router-dom';
 import { useOktaAuth } from '@okta/okta-react';
 import Login from '../views/Login';
+import Loader from './Loader';
 
 const SignIn = () => {
   const { authState } = useOktaAuth();
 
   if (authState.pending) {
-    return <p>Loading...</p>;
+    return <Loader />;
   }
 
   return authState.isAuthenticated
